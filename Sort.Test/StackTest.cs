5524049cd5aa56d0126c82e452597e1a2c0398eb
xarix <dmduda17@gmail.com>
2020-03-20 19:10:22 +0100

    Restororing to working version

---
+++ Sort.Test/StackTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Sort.Test
+{
+    class Test
+    {
+
+        [Test]
+        public void StackTestEmpty()
+        {
+            var s = new Stack(3);
+            var p = new Pair(1, 1);
+
+            s.Push(p);
+            s.Push(p);
+            s.Push(p);
+
+            s.Pop();
+            s.Pop();
+            s.Pop();
+
+            Assert.AreEqual(true, s.IsEmpty());
+        }
+
+        [Test]
+        public void StackTestException()
+        {
+            var s = new Stack(3);
+            var p = new Pair(1, 1);
+
+            s.Push(p);
+            s.Push(p);
+            s.Push(p);
+
+            s.Pop();
+            s.Pop();
+            s.Pop();
+
+        }
+    }
+}

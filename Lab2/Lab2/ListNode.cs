27659a1cb096ead2df272ad0a7c394f33f6f33f2
xarix <dmduda17@gmail.com>
2020-04-05 21:06:51 +0200

    Initial commit, created most of the methods for sorted linked list and binary search tree

---
+++ Lab2/Lab2/ListNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2
+{
+    public class ListNode
+    {
+        public int value;
+        public ListNode next;
+        public ListNode(int value)
+        {
+            this.value = value;
+        }
+    }
+}
